7c17e32a9f0818e6b9c14f24441ca33e32953869
zacharycohen <zachary.pcohen@gmail.com>
2026-04-16 13:58:48 -0400

    Create Isolate_90pcnodes_IDhitsbywindow.sh

---
+++ Isolate_90pcnodes_IDhitsbywindow.sh
@@ -0,0 +1,20 @@
+
+
+### identify_hits_by_window_Mar6.sh:
+
+WINLIST="CnanuMar6_99pc_csv"   # your CSV list
+TSV="C1_13_Feb26_input.tsv"
+
+while IFS=, read -r chr s e; do
+  awk -v chr="$chr" -v s="$s" -v e="$e" '
+    BEGIN{OFS="\t"}
+    {
+      # reorder to match your one-off output exactly
+      print $5, $3, $1, $4, $2
+    }
+  ' "$TSV" \
+  | awk -v chr="$chr" -v s="$s" -v e="$e" '
+      $1==chr && $2>=s && $2<=e
+    ' \
+  | grep "Cnanu"
+done < "$WINLIST"
